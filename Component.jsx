c764c83710e675f802fa22c17445ec3a87ce3048
Anton1oo <johosenacsantos@gmail.com>
2023-09-28 18:36:19 -0300

    28-09

--- Component.jsx
+++ Component.jsx
@@ -1,8 +1,14 @@
-import { StyleSheet, Tex, View } from 'reac-native';
-export function ComponenteTest(){
-    return{
-        <text>
-        Alura
-        </text>
-    }
+import { StyleSheet, Tex, View, Button } from 'reac-native';
+import {userState} from 'react'
+
+export function ComponenteTest({nome}){
+
+    const [data, setData]=useState();
+
+    return(
+        <View>
+        <Text>{nome}</Text>
+        <Button onClick={()=setData(9)}>Clique Aqui</Button>
+        </View>
+    );
 }
